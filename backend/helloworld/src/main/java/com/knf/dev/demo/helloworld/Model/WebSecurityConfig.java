98d55d3ce085617c552fc8fc20f7cf8d16f89ec5
khanalorama91 <149810850+khanalorama91@users.noreply.github.com>
2023-11-27 22:34:44 -0500

    update

--- backend/helloworld/src/main/java/com/knf/dev/demo/helloworld/Model/WebSecurityConfig.java
+++ backend/helloworld/src/main/java/com/knf/dev/demo/helloworld/Model/WebSecurityConfig.java
@@ -3,40 +3,49 @@ package com.knf.dev.demo.helloworld.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfiguration {
+public class WebSecurityConfig {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService; // Ensure this is the correct UserDetailsService
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
             .authorizeRequests()
-            .requestMatchers("/login").permitAll()
-            .anyRequest().authenticated()
+            	.antMatchers("/register", "/login").permitAll()
+            	.anyRequest().authenticated()
             .and()
             .formLogin()
             .loginProcessingUrl("/login")
             .permitAll();
+
+        return http.build();
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-            .passwordEncoder(passwordEncoder());
+    // If you're using the AuthenticationManager bean
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        return auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
     }
 }
