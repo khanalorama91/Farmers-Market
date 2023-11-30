92617e5304b75f923323f796bb7498c73251cd89
djd064 <diyudiggs@gmail.com>
2023-11-30 15:28:24 -0500

    Adding product category, controller, serivce and repository

---
+++ backend/helloworld/src/main/java/com/knf/dev/demo/helloworld/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.knf.dev.demo.helloworld.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+
+	@RequestMapping(value = "/products", method = RequestMethod.GET)
+	public String list(Model model) {
+		model.addAttribute("products", productService.listAllProducts());
+		return "products";
+	}
+
+	@RequestMapping(value = "product/{id}", method = RequestMethod.GET)
+	public String showProduct(@PathVariable Integer id, Model model) {
+		model.addAttribute("product", productService.getProductById(id));
+		return "productshow";
+	}
+}
