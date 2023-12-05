4a59dc311f456076acfbf180e79f83080581b0b4
khanalorama91 <149810850+khanalorama91@users.noreply.github.com>
2023-12-05 14:40:15 -0500

    mysql implementation with login and signup functionality
    mysql implementation with login and signup functionality

--- backend/helloworld/src/main/java/com/knf/dev/demo/helloworld/Model/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.knf.dev.demo.helloworld.Model;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
---
+++ backend/src/main/java/com/farmersMarket/spring/login/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.farmersMarket.spring.login.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.farmersMarket.spring.login.models.ERole;
+import com.farmersMarket.spring.login.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
